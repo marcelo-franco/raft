477e2ec6f06f89d8e45dec32564ffd3c012f5521
Marcelo Franco <marcelo.franco.br@gmail.com>
2018-12-04 12:40:49 -0200

    translation

--- visual/scripts/frames/title.js
+++ visual/scripts/frames/title.js
@@ -15,12 +15,12 @@ define([], function () {
 
         .after(500, function () {
             frame.model().title = '<h1 style="visibility:visible">Raft</h1>'
-                        + '<h2 style="visibility:visible">Understandable Distributed Consensus</h2>'
+                        + '<h2 style="visibility:visible">Consenso Distribuído Compreensível</h2>'
                         + '<br/>' + frame.model().controls.html();
             layout.invalidate();
         })
         .after(500, function () {
-            frame.model().subtitle = '<p style="visibility:visible"><em><a href="https://github.com/benbjohnson/thesecretlivesofdata/issues/1" target="_blank">Please note: this is a working draft. Click here to provide feedback.</a></em></h1>';
+            frame.model().subtitle = '<p style="visibility:visible"><em><a href="https://github.com/benbjohnson/thesecretlivesofdata/issues/1" target="_blank">Caso deseje, acesse o projeto original para dar o seu feedback.</a></em></h1>';
             layout.invalidate();
             frame.model().controls.show();
         })
